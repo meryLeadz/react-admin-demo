c3022ab7b0928135f162d011f80b29f5f60b2415
meryLeadz <105354720+meryLeadz@users.noreply.github.com>
2025-07-11 17:17:21 +0200

    PostPanel function + FunctionField

--- src/posts/post-list.tsx
+++ src/posts/post-list.tsx
@@ -1,12 +1,27 @@
-import { Datagrid, List, ReferenceField, TextField, SimpleList } from 'react-admin';
+import { Datagrid, List, ReferenceField, TextField, SimpleList, FunctionField } from 'react-admin';
+
+const PostPanel = () => {
+    return <div>Hello</div>
+}
 
 const PostList = () => (
     <List>
-        <SimpleList
-            primaryText={(record) => record.title}
-            secondaryText={(record) => record.body}
-        />
+        <Datagrid
+            expand={<PostPanel />}
+            sx={{
+                '.RaDatagrid-headerCell': {
+                    padding: '16px'
+                }
+            }}>
+            <TextField source="id" />
+            <TextField source="title" label="Post Title" />
+            <FunctionField
+                label="Excerpt"
+                render={(record) => `${record.body.substring(0, 90)}...`}
+            />
+            <ReferenceField source='usedId' reference='users' />
+        </Datagrid>
     </List>
 );
 
-export default PostList;
+export default PostList; 
